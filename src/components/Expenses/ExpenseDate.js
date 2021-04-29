07be4a2ec014df7a4b15b9f50eb5cf984fbedd2e
Richard Capraro <richard.capraro@saagie.com>
2021-04-29 20:26:33 +0200

    Add first state

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,9 +1,9 @@
-import './ExpenseDate.css';
+import './ExpenseDate.css'
 
 const ExpenseDate = (props) => {
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const year = props.date.getFullYear();
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const month = props.date.toLocaleString('en-US', { month: 'long' })
+  const year = props.date.getFullYear()
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' })
 
   return (
     <div className='expense-date'>
@@ -11,7 +11,7 @@ const ExpenseDate = (props) => {
       <div className='expense-date__year'>{year}</div>
       <div className='expense-date__day'>{day}</div>
     </div>
-  );
+  )
 }
 
-export default ExpenseDate;
+export default ExpenseDate
