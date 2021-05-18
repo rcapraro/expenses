224b5ab8fc6771bcab511967f388b8e6d79e7295
Richard Capraro <richard.capraro@gmail.com>
2021-05-18 12:51:03 +0200

    add condition on display 'Add new expense' button

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,41 @@
+import { useState } from 'react'
 import { v4 as uuidV4 } from 'uuid'
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm'
 
 const NewExpense = props => {
+  const [isEditing, setIsEditing] = useState(false)
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: uuidV4()
     }
     props.onAddExpense(expenseData)
+    setIsEditing(false)
   }
 
-  return <div className="new-expense">
-    <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
-  </div>
+  const startEditingHandler = () => {
+    setIsEditing(true)
+  }
+
+  const stopEditingHandler = () => {
+    setIsEditing(false)
+  }
+
+  return (
+    <div className="new-expense">
+      {!isEditing &&
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      }
+      {isEditing &&
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      }
+    </div>
+  )
 }
 
 export default NewExpense
